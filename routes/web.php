c89d07dd58e50e0b8c8e86c31aa9f5d774da5145
LenMaroon <79260810+LenMaroon@users.noreply.github.com>
2022-01-16 22:43:52 +1000

    Lesson 4: Add news routes, controller and views (#3)
    * Lesson 3: Add news model, migration, factory and seeder
    
    * Lesson 4: Add news routes, controller and views
    
    Co-authored-by: chrn-feip <40585429+chrn-feip@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/news', [NewsController::class, 'getList'])->name('news_list');
+
+Route::get('/news/{slug}', [NewsController::class, 'getDetails'])->name('news_item');
