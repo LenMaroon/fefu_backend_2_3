3da0599e5f02ce03b3d5f4489647095ce5b9945c
LenMaroon <79260810+LenMaroon@users.noreply.github.com>
2022-01-16 22:39:30 +1000

    Lesson 3: Add news model, migration, factory and seeder (#2)

---
+++ database/factories/NewsFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\News;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NewsFactory extends Factory
+{
+    protected $model = News::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+    public function definition() {
+        return [
+            'title' => $this->faker->unique->sentence(),
+            'description' => $this->faker->realTextBetween(250, 400),
+            'text' => $this->faker->realTextBetween(1000, 1500),
+            'is_published' => $this->faker->boolean(70),
+            'published_at' => $this->faker->dateTimeBetween('-2 months')
+        ];
+    }
+}
